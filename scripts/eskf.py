6445603c8d5eb35ecede95c792f0d9cbee39df46
Yuta Suzuki <suzuki.y.ee@m.titech.ac.jp>
2024-12-04 17:01:26 +0900

    システム方程式を追加

--- scripts/eskf.py
+++ scripts/eskf.py
@@ -1,6 +1,11 @@
+#
+# https://www.flight.t.u-tokyo.ac.jp/?p=800 を参照
+#   - ローカルな角度誤差
+#
+
 from cobs import cobs_decode
-from tqdm import tqdm
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 from struct import unpack
 import numpy as np
 
@@ -24,14 +29,15 @@ if __name__=="__main__":
     R=np.eye(3)
     x=np.zeros(19) #  [p,v,q,ab,wb,g] : State variable
     ex=np.zeros(18) # [ep,ev,etheta,eab,ewb,eg] : Error-State variable
-    P=np.zeros((18,18))
+    P=np.zeros((18,18)) # Covariance matrix
     sigma_v=0.1**2*dt_m**2
     sigma_theta=1e-8**2*dt_m**2
     sigma_a=1e-3**2*dt_m
     sigma_w=5e-3**2*dt_m
-    Q=np.diag([sigma_v,sigma_v,sigma_v,sigma_theta,sigma_theta,sigma_theta,sigma_a,sigma_a,sigma_a,sigma_w,sigma_w,sigma_w])
-    Fx=np.eye(18)
-    R=np.zeros((3,3))
+    Q=np.diag([sigma_v,sigma_v,sigma_v,sigma_theta,sigma_theta,sigma_theta,sigma_a,sigma_a,sigma_a,sigma_w,sigma_w,sigma_w]) # Process noise covariance
+    Fx=np.eye(18) # State transition matrix
+
+    R=np.zeros((3,3)) # R[q] : Rotation matrix
 
     Fi=np.zeros((18,12))
     # (270)
@@ -40,7 +46,7 @@ if __name__=="__main__":
     Fx[0:3,3:6]=np.eye(3)*dt_m
     Fx[3:6,15:18]=np.eye(3)*dt_m
 
-    # z=[re,rn,ve,vn]
+    # z=[re,rn,ve,vn] : observation variable
     Hx=np.zeros((4,19))
     Hx[0:2,0:2]=np.eye(2)
     Hx[2:4,3:5]=np.eye(2)
@@ -75,6 +81,17 @@ if __name__=="__main__":
                     am=np.array([ax,ay,az])
                     wm=np.array([wx,wy,wz])
 
+                    # System dynamics
+                    p+=v*dt_m+0.5*(R@(am-ab)-g)*dt_m**2
+                    v+=R@(am-ab)-g*dt_m
+                    q+=0.5*np.array([
+                        [-q[1],-q[2],-q[3]],
+                        [ q[0],-q[3],q[2]],
+                        [ q[3],q[0],-q[1]],
+                        [-q[2],q[1], q[0]]
+                        ])@wb*dt_m
+                    # wb, ab, g : not updated
+
                     # calculate (270)
                     Fx[3:6,6:9]=-R@np.array([[             0,-(am[2]-ab[2]), (am[1]-ab[1])],
                                              [ (am[2]-ab[2]),             0,-(am[0]-ab[0])],
@@ -83,7 +100,7 @@ if __name__=="__main__":
                     Fx[6:9,6:9]=R.T@np.diag(wm-wb)*dt_m
 
                     #ex=Fx@ex # (268)
-                    P=Fx@P@(Fx.T)+Fi@Q@(Fi.T) #(269)
+                    P=Fx@P@(Fx.T)+Fi@Q@(Fi.T) #(269) : Covariance update
 
                 case 0x60:
                     timestamp,t,lat,lng,alt,ve,vn=unpack(">IxxxxIddfffxxxx",bytes(dec))
@@ -95,6 +112,7 @@ if __name__=="__main__":
                     wb=x[13:16].copy()
                     g=x[16:19].copy()
 
+                    # (279) ~ (281) : the Jacobian of the observation model
                     Xex[6:10,6:9]=0.5*np.array([
                         [-q[1],-q[2],-q[3]],
                         [ q[0],-q[3],q[2]],
@@ -105,6 +123,8 @@ if __name__=="__main__":
                     H= Hx@Xex
                     K = P@(H.T)@np.linalg.inv(H@P@(H.T)+np.diag([2.5,2.5,0.1,0.1]))
                     ex = K@(np.array([0,0,0,0])-H@ex)
+                    
+                    # (282) : nominal state update
                     x[0:6]+=ex[0:6]
                     x[10:19]=ex[9:18]
                     ex[:]=0
