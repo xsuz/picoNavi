c14995589b59d08a74a9c3c768e7c26ccdba900e
Yuta Suzuki <suzuki.y.ee@m.titech.ac.jp>
2025-01-02 14:59:39 +0900

    ファイル名のバグ解消

--- picoNavi/src/sd_logger.cpp
+++ picoNavi/src/sd_logger.cpp
@@ -97,7 +97,7 @@ namespace sd_logger
         {
             if ((year % 4 == 0) && ((year % 400==0) || !(year % 100==0)))
             {
-                if(t-unixtime<=366){
+                if(t-unixtime<366){
                     break;
                 }else{
                     year++;
